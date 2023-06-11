cb54666ba9d69768a0238214eef27ea4cfbe6ae9
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-06-11 14:03:15 +0200

    Added CLI template

---
+++ dp.py
@@ -0,0 +1,28 @@
+import argparse
+
+argumentParser = argparse.ArgumentParser() # Initialize parser
+
+# Positional arguments
+argumentParser.add_argument('victim')
+
+# Optional arguments
+argumentParser.add_argument('-a', '--arp', action = 'store_true') # ARP Poisoning Attack Flag
+argumentParser.add_argument('-d', '--dns', action = 'store_true') # DNS Spoofing Attack Flag
+argumentParser.add_argument('-s', '--ssl', action = 'store_true') # SSL Stripping Attack Flag
+
+# Parse arguments
+parseArgs = argumentParser.parse_args()
+victim = parseArgs.victim 
+isARPPoisoningAttack = parseArgs.arp
+isDNSStrippingAttack = parseArgs.dns
+isSSLStrippingAttack = parseArgs.ssl
+
+# Start right attack
+if isARPPoisoningAttack:
+    print('ARP Poisoning Attack has been started')
+
+if isDNSStrippingAttack:
+    print('DNS Spoofing Attack has been started')
+
+if isSSLStrippingAttack:
+    print('SSL Stripping Attack has been started')
